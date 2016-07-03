b949992419fdaf72ae06e44d1e2ea456802f1e45
alxe_master <alxe.master@gmail.com>
2016-07-03 20:33:19 +0300

    add app logic

--- index.js
+++ index.js
@@ -1,30 +1,68 @@
 'use strict';
 
 
-var five = require("johnny-five");
-var board = new five.Board();
-var _ = require('lodash');
+const five = require('johnny-five');
+const board = new five.Board();
+const _ = require('lodash');
 
-board.on("ready", function() {
-    // Create an Led on pin 13
+const wait = (board, hourCount, handler) => {
+    let active = true;
+
+    board.wait(hourCount * 60 * 60 * 1000, () => {
+        if (!active) {
+            return;
+        }
+
+        handler();
+    });
+
+    return () => {
+        active = false;
+    };
+};
+
+const Loop = (board, base, handlers, done) =>
+    () => {
+        const offs = handlers.map((handler, index) =>
+            wait(board, base * (index + 1), () => {
+                handler();
+            })
+        );
+
+        return () => {
+            offs.forEach((off) => {
+                off();
+            });
+
+            done();
+        }
+    };
+
+board.on('ready', () => {
     var button = new five.Button(2);
     var led1 = new five.Led(11);
     var led2 = new five.Led(13);
 
-    led2.blink(1500);
+    led2.off();
     led1.off();
 
-    var state = false;
-
-    button.on("hold", function() {
-        state = !state;
-
-        console.log(111, Date.now());
-
-        if (state) {
-            led1.on();
-        } else {
-            led1.off();
+    const loop = Loop(board, 0.5, [
+        () => {
+            led1.blink(2000);
+        },
+        () => {
+            led2.blink(1000);
         }
+    ], () => {
+        led2.off();
+        led1.off();
+    });
+
+    let offLoop = loop();
+
+    button.on('hold', () => {
+        offLoop();
+
+        offLoop = loop();
     });
 });
